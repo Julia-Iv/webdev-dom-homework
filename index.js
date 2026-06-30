7a3accf142404db85a8707f6680b9a7c75ba5f05
Julia <bogatayadomo@gmail.com>
2026-06-30 19:58:12 +0300

    dom5-3

--- index.js
+++ index.js
@@ -2,16 +2,31 @@ import { initAddCommentForm } from "./modules/addCommentForm.js";
 import { updateComments } from "./modules/comments.js";
 import { renderComments, initToggleLikeListener } from "./modules/renderComments.js"
 
+
 initAddCommentForm();
-initToggleLikeListener();
+//initToggleLikeListener();
 //renderComments();
 
 
 fetch('https://wedev-api.sky.pro/api/v1/Julia-Iv/comments')
 .then((response) => {
-    return response.json()
+    return response.json();
+    
 })
-.then((data) => {
-  updateComments(data.comments);
+.then((responseData) => {
+      return responseData.comments.map ((comment) => {
+        return {
+          id: comment.id,
+          likesCount: comment.likes,
+          isLiked: comment.isLiked,
+          name: comment.author.name,
+          text: comment.text,
+          data: new Date(comment.date).toLocaleString() 
+        }
+      })
+    })
+.then ((appComments) => {
+  updateComments(appComments);
   renderComments();
-})
+  initToggleLikeListener();
+});
